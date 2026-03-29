6e73e1bf62380fb41d9b1df0639a79f06c323efa
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2026-03-29 19:23:14 -0300

    fix:proxy

--- next-poc/proxy.ts
+++ next-poc/proxy.ts
@@ -10,13 +10,13 @@ export async function proxy(request: NextRequest) {
     // THIS IS NOT SECURE!
     // This is the recommended approach to optimistically redirect users
     // We recommend handling auth checks in each page/route
-    if(!session) {
-        return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
+    // if(!session) {
+    //     return NextResponse.redirect(new URL("/login", request.url));
+    // }
 
     return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard"], // Specify the routes the middleware applies to
+  //matcher: ["/dashboard"], // Specify the routes the middleware applies to
 };
